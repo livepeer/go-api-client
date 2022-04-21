34b48d3d471d6d11f0e123cc0615e078e8327c13
Tony Qian <t.qian7@gmail.com>
2022-04-21 15:14:04 +0200

    rename to PlaybackRecordingID

--- api.go
+++ api.go
@@ -222,12 +222,12 @@ type (
 	}
 
 	AssetSpec struct {
-		Name                       string          `json:"name,omitempty"`
-		Type                       string          `json:"type"`
-		Size                       uint64          `json:"size"`
-		Hash                       []AssetHash     `json:"hash"`
-		VideoSpec                  *AssetVideoSpec `json:"videoSpec,omitempty"`
-		PlaybackRecordingSessionID string          `json:"playbackRecordingSessionId,omitempty"`
+		Name                string          `json:"name,omitempty"`
+		Type                string          `json:"type"`
+		Size                uint64          `json:"size"`
+		Hash                []AssetHash     `json:"hash"`
+		VideoSpec           *AssetVideoSpec `json:"videoSpec,omitempty"`
+		PlaybackRecordingID string          `json:"playbackRecordingId,omitempty"`
 	}
 
 	AssetHash struct {
